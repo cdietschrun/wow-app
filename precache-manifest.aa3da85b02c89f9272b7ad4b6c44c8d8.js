d86414586aef70b96a3784890eef64782c7c5c4c
Chris Dietsch <chdietsc@microsoft.com>
2020-10-08 00:14:53 -0400

    Updates

---
+++ precache-manifest.aa3da85b02c89f9272b7ad4b6c44c8d8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "544af97b54d21c5576b26ba6e37a6eaf",
+    "url": "/wow-app/index.html"
+  },
+  {
+    "revision": "c8bb85edfb4883e28e33",
+    "url": "/wow-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "174becba5017cbe97fa2",
+    "url": "/wow-app/static/js/2.145ff0ab.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/wow-app/static/js/2.145ff0ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8bb85edfb4883e28e33",
+    "url": "/wow-app/static/js/main.393f0f0f.chunk.js"
+  },
+  {
+    "revision": "04eb56e654a0c4e4ad7e",
+    "url": "/wow-app/static/js/runtime-main.379f1cc9.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/wow-app/precache-manifest.c1c039b4f765d509583183a26517e2b0.js"
+  "/wow-app/precache-manifest.aa3da85b02c89f9272b7ad4b6c44c8d8.js"
 );
 
 self.addEventListener('message', (event) => {
