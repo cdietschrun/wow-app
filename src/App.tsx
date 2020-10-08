6b3a7f83951ac817278d8fe7ab53001a442521bf
Chris Dietsch <chdietsc@microsoft.com>
2020-10-07 22:25:51 -0400

    updtes working locally

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,37 @@
-import React from 'react';
+import * as React from "react";
 import logo from './logo.svg';
 import './App.css';
+import { useState } from "react";
 
-function App() {
+const BlizzAPI = require('blizzapi');
+
+function App()
+{
+  const [characterHeader, setCharacterHeader] = useState({} as any);
+
+  React.useEffect(() => {
+    getCharacterData();
+  }, []);
+
+  async function getCharacterData()
+  {
+    const BnetApi = new BlizzAPI({region:'us',
+                                  clientId: 'fcfe325a7c74445e8e7f5a27ca66520f', 
+                                  clientSecret: 'kOrHT94U3YWLwr9cx874ui3H8t5Ok4jK' });
+    const accessToken = await BnetApi.getAccessToken();
+    console.log(accessToken);
+
+    const character_header = await BnetApi.query('/profile/wow/character/zuljin/tyegath', 
+    { headers: { 'Battlenet-Namespace':'profile-us' } });
+    setCharacterHeader(character_header);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Chri22 Edit <code>src/App.js</code> and save to reload. points: {characterHeader['achievement_points']}
         </p>
         <a
           className="App-link"
