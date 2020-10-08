d0f5c273aa5d7de3f2c839ed2b08feed6158acdc
Chris Dietsch <chdietsc@microsoft.com>
2020-10-07 20:50:11 -0400

    Updates

---
+++ precache-manifest.9333dedfe724a676e884e46109c93e54.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a6449b023a6334f1ba1346febaad352",
+    "url": "/wow-app/index.html"
+  },
+  {
+    "revision": "8b6e84a6d14eef17ca44",
+    "url": "/wow-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "08e90c354a5a920b192c",
+    "url": "/wow-app/static/js/2.ca765737.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/wow-app/static/js/2.ca765737.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b6e84a6d14eef17ca44",
+    "url": "/wow-app/static/js/main.29c6efdf.chunk.js"
+  },
+  {
+    "revision": "04eb56e654a0c4e4ad7e",
+    "url": "/wow-app/static/js/runtime-main.379f1cc9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/wow-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
